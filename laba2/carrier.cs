1dd8ab13be3b263414fabac16de45d3929780761
Tomashchik Daniil <tomashchik032@gmail.com>
2020-11-11 12:54:59 +0300

    lab2

---
+++ laba2/carrier.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+namespace laba2
+{
+    class carrier
+    {
+
+        public string Source { get; set; }
+
+        public string Target { get; set; }
+
+        public string FileName { get; set; }
+
+        public FileInfo Info { get; set; }
+
+
+
+        public carrier(string source, string target, string fileName)
+        {
+            this.Source = source;
+            this.Target = target;
+            this.FileName = fileName;
+            this.Info = new FileInfo(source + fileName);
+        }
+
+        public void addFile()
+        {
+            string path = this.CreateFolders();
+            string newFileName = $"\\Sales_{Info.CreationTime.Year}_{Info.CreationTime.Month}_{Info.CreationTime.Day}_{Info.CreationTime.Hour}" +
+                $"_{Info.CreationTime.Minute}_{Info.CreationTime.Second}";
+            string fullPath = path + newFileName;
+            File.Copy(Source + FileName, fullPath, true);
+            File.Delete(Source + FileName);
+            (string cryptPath, string cryptName) = crypt.CryptFile(fullPath);
+            File.Delete(fullPath);
+            string compressSource = compressor.compress(cryptPath, cryptName);
+            compressor.Decompress(compressSource, cryptName);
+        }
+
+
+        public string CreateFolders()
+        {
+            string year = Info.CreationTime.Year.ToString();
+            string month = Info.CreationTime.Month.ToString();
+            string day = Info.CreationTime.Day.ToString();
+            string path = $"{Target}\\{year}\\{month}\\{day}";
+            Directory.CreateDirectory(path);
+            Directory.CreateDirectory("C:\\Target_Text\\Archive");
+            return path;
+        }
+    }
+}
---
+++ laba2/crypt.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+namespace laba2
+{
+    class crypt
+    {
+
+        const string archivePath = "C:\\Target_Text\\Archive";
+        public static (string, string) CryptFile(string path)
+        {
+            byte[] fileBytes = File.ReadAllBytes(path);
+            for (int i = 0; i < fileBytes.Length; i++)
+            {
+                fileBytes[i] ^= 1;
+            }
+
+
+            string newPath = path.EndsWith(".crypt") ? path.Remove(path.LastIndexOf(".crypt")) : path + ".crypt";
+            string newName = newPath.Split((char)92)[newPath.Split((char)92).Length - 1];
+            File.WriteAllBytes(newPath, fileBytes);
+            return (newPath, newName);
+        }
+
+
+        public static void DecryptFile(string path)
+        {
+            byte[] fileBytes = File.ReadAllBytes(path);
+            for (int i = 0; i < fileBytes.Length; i++)
+            {
+                fileBytes[i] ^= 1;
+            }
+            string newPath = path.EndsWith(".txt") ? path.Remove(path.LastIndexOf(".txt")) + "(Decrypted).txt" : path + "(Decrypted).txt";
+            File.WriteAllBytes(newPath, fileBytes);
+        }
+
+    }
+}
